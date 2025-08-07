1843c6e28747441fe4fb9e416df23a771cedda9e
dina <20Ahmed.Ammar10@gmail.com>
2025-08-07 20:58:23 +0300

    update

--- src/pages/customer/allProducts.jsx
+++ src/pages/customer/allProducts.jsx
@@ -40,7 +40,6 @@ export default function AllProducts() {
 
   return (
     <div className="animate-slideInFromLeft p-6 m-10 max-w-7xl mx-auto ">
-      <h2 className="text-2xl font-bold mb-6">كل المنتجات</h2>
 
       {products.length === 0 && (
         <p className="text-center text-gray-500">لا توجد منتجات</p>
@@ -50,42 +49,42 @@ export default function AllProducts() {
         {products.map((product) => (
           <div className="relative group bg-white rounded-lg shadow-md overflow-hidden cursor-pointer">
 
-  <img
-    src={product.image || "https://via.placeholder.com/300"}
-    alt={product.name}
-    className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105"
-  />
-
-  
-  <div className="absolute inset-0 bg-black opacity-20 group-hover:opacity-40 transition-opacity duration-300 pointer-events-none z-20"></div>
+            <img
+              src={product.image || "https://via.placeholder.com/300"}
+              alt={product.name}
+              className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105"
+            />
 
 
-  <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-30 pointer-events-auto">
-    <Link
-      to={`/product/${product._id}`}
-      className="text-yellow-700 px-5 py-2 rounded shadow-lg font-bold hover:text-yellow-500"
-    >
-      عرض التفاصيل
-    </Link>
-  </div>
+            <div className="absolute inset-0 bg-black opacity-20 group-hover:opacity-40 transition-opacity duration-300 pointer-events-none z-20"></div>
 
 
-  <div className="p-4 bg-white relative z-40">
-    <h3 className="text-lg font-semibold mb-1">{product.name}</h3>
-    <p className="text-gray-700 font-bold mb-3">{product.price} جنيه</p>
-    <button
-      onClick={() => {
-        console.log("Adding product to cart:", product);
-        toast.success("✅ تم إضافة المنتج إلى السلة");
-        addToCart(product);
-      }}
-      className="text-yellow-700 font-bold hover:text-yellow-500 transition duration-300"
-    >
-      أضف إلى السلة
-    </button>
-  </div>
+            <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-30 pointer-events-auto">
+              <Link
+                to={`/product/${product._id}`}
+                className="text-yellow-700 px-5 py-2 rounded shadow-lg font-bold hover:text-yellow-500"
+              >
+                عرض التفاصيل
+              </Link>
+            </div>
 
-</div>
+
+            <div className="p-4 bg-white relative z-40">
+              <h3 className="text-lg font-semibold mb-1">{product.name}</h3>
+              <p className="text-gray-700 font-bold mb-3">{product.price} جنيه</p>
+              <button
+                onClick={() => {
+                  console.log("Adding product to cart:", product);
+                  toast.success("✅ تم إضافة المنتج إلى السلة");
+                  addToCart(product);
+                }}
+                className="text-yellow-700 font-bold hover:text-yellow-500 transition duration-300"
+              >
+                أضف إلى السلة
+              </button>
+            </div>
+
+          </div>
 
 
         ))}
